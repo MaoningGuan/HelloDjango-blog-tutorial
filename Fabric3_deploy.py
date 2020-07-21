99469392ebe9d8a1a096001b5959395db669499b
Maoning Guan <1812711281@qq.com>
2020-07-21 20:36:50 +0800

    申请 HTTPS 证书

--- Fabric3_deploy.py
+++ Fabric3_deploy.py
@@ -2,7 +2,10 @@
 """
 使用 Fabric 编写自动部署脚本
 """
-from fabric import task
+# 1.  创建一个远程连接
+# 2. 进入指定目录
+# 3. 在指定目录下面执行重启命令
+from fabric import Connection
 from invoke import Responder
 from _credentials import github_username, github_password
 
@@ -22,12 +25,14 @@ def _get_github_auth_responders():
     return [username_responder, password_responder]
 
 
-@task()
-def deploy(c):
+def deploy():
     supervisor_conf_path = '~/etc/'
     supervisor_program_name = 'hellodjango-blog-tutorial'
 
     project_root_path = '~/apps/HelloDjango-blog-tutorial/'
+
+    # 如果你的电脑配了ssh免密码登录，就不需要 connect_kwargs 来指定密码了。
+    c = Connection("root@106.53.196.251", connect_kwargs={"password": "Mm187020"})
 
     # 先停止应用
     with c.cd(supervisor_conf_path):
@@ -50,3 +55,7 @@ def deploy(c):
     with c.cd(supervisor_conf_path):
         cmd = 'supervisorctl -c ~/etc/supervisord.conf start {}'.format(supervisor_program_name)
         c.run(cmd)
+
+
+if __name__ == '__main__':
+    deploy()
