b12ecbf7c759ee5442b7970981120b627a8f3901
Maoning Guan <1812711281@qq.com>
2020-07-22 22:19:13 +0800

    更新Fabric 2.0自动化部署的代码。

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-使用 Fabric 编写自动部署脚本
+使用 Fabric 2.0 编写自动部署脚本
 """
-from fabric import task
+# 1.  创建一个远程连接
+# 2. 进入指定目录
+# 3. 在指定目录下面执行重启命令
+from fabric import Connection
 from invoke import Responder
-from _credentials import github_username, github_password
+from _credentials import github_username, github_password, server_host_ip, server_username, server_password
 
 
 def _get_github_auth_responders():
@@ -22,16 +25,18 @@ def _get_github_auth_responders():
     return [username_responder, password_responder]
 
 
-@task()
-def deploy(c):
-    supervisor_conf_path = '~/etc/'
+def deploy():
+    supervisor_conf_path = '/home/gmn/etc/'
     supervisor_program_name = 'hellodjango-blog-tutorial'
 
-    project_root_path = '~/apps/HelloDjango-blog-tutorial/'
+    project_root_path = '/home/gmn/apps/HelloDjango-blog-tutorial/'
+
+    # 如果你的电脑配了ssh免密码登录，就不需要 connect_kwargs 来指定密码了。
+    c = Connection(f"{server_username}@{server_host_ip}", connect_kwargs={"password": server_password})
 
     # 先停止应用
     with c.cd(supervisor_conf_path):
-        cmd = 'supervisorctl -c ~/etc/supervisord.conf stop {}'.format(supervisor_program_name)
+        cmd = 'supervisorctl -c /home/gmn/etc/supervisord.conf stop {}'.format(supervisor_program_name)
         c.run(cmd)
 
     # 进入项目根目录，从 Git 拉取最新代码
@@ -48,5 +53,9 @@ def deploy(c):
 
     # 重新启动应用
     with c.cd(supervisor_conf_path):
-        cmd = 'supervisorctl -c ~/etc/supervisord.conf start {}'.format(supervisor_program_name)
+        cmd = 'supervisorctl -c /home/gmn/etc/supervisord.conf start {}'.format(supervisor_program_name)
         c.run(cmd)
+
+
+if __name__ == '__main__':
+    deploy()
